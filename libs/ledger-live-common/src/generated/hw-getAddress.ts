bdcef05128894275502ab3a416b1c18ca3039e1b
Troels F. Rønnow <troels@wonop.com>
2023-03-15 15:35:53 +0100

    Fixing Zilliqa integration tests

--- libs/ledger-live-common/src/generated/hw-getAddress.ts
+++ libs/ledger-live-common/src/generated/hw-getAddress.ts
@@ -17,7 +17,6 @@ import tezos from "../families/tezos/hw-getAddress";
 import tron from "../families/tron/hw-getAddress";
 import zilliqa from "../families/zilliqa/hw-getAddress";
 import polkadot from "@ledgerhq/coin-polkadot/hw-getAddress";
-import zilliqa from "../families/zilliqa/hw-getAddress";
 
 export default {
   algorand,
@@ -39,5 +38,4 @@ export default {
   tron,
   zilliqa,
   polkadot,
- zilliqa
 };
--- libs/ledger-live-common/src/generated/transaction.ts
+++ libs/ledger-live-common/src/generated/transaction.ts
@@ -38,5 +38,4 @@ export default {
   tron,
   zilliqa,
   polkadot,
- zilliqa
 };
